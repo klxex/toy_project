ffd7ae1d49ff37039ca89d8b8db3eab0c5d00467
klxex <hwanguebest@naver.com>
2021-09-16 11:00:05 +0900

    create project

---
+++ yaksa/src/main/java/com/hwan/yaksa/YaksaApplication.java
@@ -0,0 +1,13 @@
+package com.hwan.yaksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaksaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaksaApplication.class, args);
+	}
+
+}
---
+++ yaksa/src/test/java/com/hwan/yaksa/YaksaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwan.yaksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaksaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
